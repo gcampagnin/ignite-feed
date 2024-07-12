64f2fbb406417290c24486b05007881a61522116
Gustavo Campagnin <gcampagnin@gmail.com>
2024-07-12 18:08:52 -0300

    feat: add post

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import { Header } from "./components/Header"
 import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post"
 
 import "./global.css"
 
 import styles from "./App.module.css"
 
-function App() {
+export function App() {
   return (
     <div>
 			<Header/>
 
 			<div className={styles.wrapper}>
 				<Sidebar />
-				<main></main>
+				<main>
+					<Post />
+					<Post />
+				</main>
 			</div>
     </div>
   )
 }
-
-export default App
