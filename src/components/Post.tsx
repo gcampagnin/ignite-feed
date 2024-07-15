24de8d7454cdc4d7b932e9c911a93300ca1cf8c9
Gustavo Campagnin <gcampagnin@gmail.com>
2024-07-15 19:11:08 -0300

    feat: add Avatar

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css"
 
@@ -6,10 +7,8 @@ export function Post() {
 		<article className={styles.post}>
 			<header>
 				<div className={styles.author}>
-					<img
-						className={styles.avatar}
-						src="https://avatars.githubusercontent.com/u/9133583?v=4"
-					/>
+					<Avatar src="https://avatars.githubusercontent.com/u/9133583?v=4" />
+
 					<div className={styles.authorInfo}>
 						<strong>Gustavo Campagnin</strong>
 						<span>Web Developer</span>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { PencilLine } from "phosphor-react"
 
 import styles from "./Sidebar.module.css"
+import { Avatar } from "./Avatar"
 
 export function Sidebar() {
 	return (
@@ -11,10 +12,7 @@ export function Sidebar() {
 			/>
 
 			<div className={styles.profile}>
-				<img
-					className={styles.avatar}
-					src="https://avatars.githubusercontent.com/u/9133583?v=4"
-				/>
+				<Avatar src="https://avatars.githubusercontent.com/u/9133583?v=4" />
 
 				<strong>Gustavo Campagnin</strong>
 				<span>Web Developer</span>
